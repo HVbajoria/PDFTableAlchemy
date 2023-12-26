db3a20f803017f0d4e89cc6db5e9d64859658245
Harshavardhan Bajoria <62978274+HVbajoria@users.noreply.github.com>
2023-12-26 14:19:01 +0000

    Vesion 1.1

--- main.py
+++ main.py
@@ -5,6 +5,48 @@ from io import BytesIO
 import tabula
 import os
 
+hide_default_format = """ 
+        <style> 
+        footer {visibility: hidden;} 
+        </style> 
+        """ 
+st.markdown(hide_default_format, unsafe_allow_html=True) 
+
+def gradient_text(text, color1, color2):
+    gradient_css = f"""
+        background: -webkit-linear-gradient(left, {color1}, {color2});
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        font-weight: bold;
+        font-size: 42px;
+    """
+    return f'<span style="{gradient_css}">{text}</span>'
+
+def gradient(text, color1, color2):
+    gradient_css = f"""
+        background: -webkit-linear-gradient(left, {color2}, {color1});
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        font-weight: bold;
+        font-size: 22px;
+    """
+    return f'<span style="{gradient_css}">{text}</span>'
+
+color1 = "#0d3270"
+color2 = "#0fab7b"
+text = "PDFTableAlchemy"
+  
+# left_co, cent_co,last_co = st.columns(3)
+# with cent_co:
+#     st.image("images/logo.png", width=200)
+
+styled_text = gradient_text(text, color1, color2)
+st.write(f"<div style='text-align: center;'>{styled_text}</div>", unsafe_allow_html=True)
+
+text="Transforming PDF chaos into Excel gold with Table Alchemy!"
+styled_text = gradient(text, color1, color2)
+st.write(f"<div style='text-align:center;'>{styled_text}</div>",unsafe_allow_html=True)
+
 # Function to extract tables from PDF and create a zip archive
 def extract_and_zip(pdf_file):
     tables = tabula.read_pdf(pdf_file, pages='all', multiple_tables=True)
@@ -25,7 +67,6 @@ def extract_and_zip(pdf_file):
     return zip_buffer
 
 def main():
-    st.title("PDF Table Extractor and Zip Download")
 
     pdf_file = st.file_uploader("Upload a PDF file", type=["pdf"])
 
@@ -40,9 +81,32 @@ def main():
             st.dataframe(table)
 
         # Download button
-        if st.button('Download Tables as Zip'):
-            zip_buffer = extract_and_zip(pdf_file)
-            st.download_button(label='Download Zip', data=zip_buffer, file_name='tables.zip', key='download_button')
+        zip_buffer = extract_and_zip(pdf_file)
+        st.sidebar.download_button(label='Download The Files', data=zip_buffer, file_name='tables.zip', key='download_button')
 
 if __name__ == "__main__":
     main()
+
+footer="""<style>
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: white;
+color: black;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Developed with ❤️ by <a style='display: inline; text-align: center;' href="https://www.linkedin.com/in/harshavardhan-bajoria/" target="_blank">Harshavardhan Bajoria</a></p>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
